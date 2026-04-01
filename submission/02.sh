395b63fa0554264fb9629ef69eb2914cd7f7f1a8
MAC <mac@MACs-MacBook-Pro.local>
2026-04-01 20:05:27 +0100

    exercise-3

--- submission/02.sh
+++ submission/02.sh
@@ -1,3 +1,3 @@
 # Create a native segwit address and get the public key from the address.
-bitcoin-cli  -rpcwallet=builderswallet getnewaddress "" "bech32"
-bitcoin-cli -rpcwallet=builderswallet getaddressinfo tb1qpezv098xkhtg6r8tjkwue2w72k5ahslnmlp0fu
+ADDRESS=$(bitcoin-cli -regtest -rpcwallet=builderswallet getnewaddress "" "bech32")
+bitcoin-cli -regtest -rpcwallet=builderswallet getaddressinfo "$ADDRESS" | jq -r '.pubkey'
